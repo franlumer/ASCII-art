68f6909eda1f93871f765da94a6e1b0acb608cef
franlumer <[6~franlumer09@gmail.com>
2026-06-09 03:52:45 +0000

    .

--- export.sh
+++ export.sh
@@ -2,4 +2,4 @@
 
 file=$1
 
-convert -background black -fill white -font Courier -interline-spacing -6 -pointsize 20 -size 800x label:@$file.txt $file.png
+convert -background black -fill white -font Courier -interline-spacing -6 -pointsize 20 -size 800x label:@$file output2.png
